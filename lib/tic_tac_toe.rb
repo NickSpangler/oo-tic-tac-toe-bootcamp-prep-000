cb673524d5e831c0efff98c4f4e540ed5c02181d
Nick Spangler <spangleyman@mac.com>
2020-06-15 19:38:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,5 +63,15 @@ class TicTacToe
     token
   end
 
-  
+  def turn
+  puts "Please enter 1-9:"
+    input = gets.chomp
+    index = input_to_index(input)
+      if valid_move?(index)
+        move(index, current_player
+        display_board
+      else
+        turn
+      end
+  end
 end
